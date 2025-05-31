78cd9f095441ac0991c458d9a580cf47f3b0bab5
Abdelali Duiche <abdodui70@gmail.com>
2025-05-31 20:51:27 +0100

    Add ExpenseBucket model to represent Expenses by Category in the App chart.

--- lib/models/expense_model.dart
+++ lib/models/expense_model.dart
@@ -10,10 +10,10 @@ const uuid = Uuid();
 enum Category { food, travel, leisure, work }
 
 const categoryIcons = {
-  Category.food : Icons.fastfood_rounded,
-  Category.travel : Icons.flight_takeoff_rounded,
-  Category.leisure : Icons.movie_creation_rounded,
-  Category.work : Icons.work
+  Category.food: Icons.fastfood_rounded,
+  Category.travel: Icons.flight_takeoff_rounded,
+  Category.leisure: Icons.movie_creation_rounded,
+  Category.work: Icons.work,
 };
 
 class Expense {
@@ -27,10 +27,30 @@ class Expense {
     required this.title,
     required this.amount,
     required this.date,
-    required this.category
+    required this.category,
   }) : id = uuid.v4();
 
   String get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+    : expenses =
+          allExpenses.where((expense) => expense.category == category).toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
   }
 }
